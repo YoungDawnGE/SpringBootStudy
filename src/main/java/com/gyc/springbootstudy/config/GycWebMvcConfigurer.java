9821052a8ac075821f5c7586ae7fc80596982e45
ge yangchen <geyangchengyc@163.com>
2020-11-28 08:47:48 +0800

    增加国际化的配置文件 代码见OneNote

--- src/main/java/com/gyc/springbootstudy/config/GycWebMvcConfigurer.java
+++ src/main/java/com/gyc/springbootstudy/config/GycWebMvcConfigurer.java
@@ -1,7 +1,6 @@
 package com.gyc.springbootstudy.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
